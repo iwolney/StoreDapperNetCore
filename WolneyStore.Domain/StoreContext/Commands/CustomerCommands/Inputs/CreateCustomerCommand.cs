ba19d3952295351372328a431726599f9675c643
Wolney Rodrigues <wolney.design@gmail.com>
2021-10-25 19:21:47 -0300

    Add FailFastValidation. Objeto: CreateCustommerCommand

--- WolneyStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
+++ WolneyStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentValidator;
+using FluentValidator.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +8,25 @@ using System.Threading.Tasks;
 
 namespace WolneyStore.Domain.StoreContext.Commands.CustomerCommands.Inputs
 {
-    public class CreateCustomerCommand
+    public class CreateCustomerCommand : Notifiable
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Document { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        public bool Valid()
+        {
+            AddNotifications(new ValidationContract()
+                .HasMinLen(FirstName, 3, "FirstName", "O nome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(FirstName, 40, "FirstName", "O nome deve conter no máximo 40 caracteres")
+                .HasMinLen(LastName, 3, "LastName", "O sobrenome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(LastName, 40, "LastName", "O sobrenome deve conter no máximo 40 caracteres")
+                .IsEmail(Email, "Email", "O E-mail é inválido")
+                .HasLen(Document, 11, "Document", "CPF inválido")
+            );
+            return Valid();
+        }
     }
 }
