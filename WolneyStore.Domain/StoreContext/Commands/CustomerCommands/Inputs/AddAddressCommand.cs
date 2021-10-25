2c46be93d962b1ae3114da11648c2af53a4234db
Wolney Rodrigues <wolney.design@gmail.com>
2021-10-25 19:15:53 -0300

    Inicio da criação dos commands

---
+++ WolneyStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/AddAddressCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WolneyStore.Domain.StoreContext.Enums;
+
+namespace WolneyStore.Domain.StoreContext.Commands.CustomerCommands.Inputs
+{
+    public class AddAddressCommand
+    {
+        public Guid Id { get; set; }
+        public string Street { get; private set; }
+        public string Number { get; private set; }
+        public string Complement { get; private set; }
+        public string District { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string Country { get; private set; }
+        public string ZipCode { get; private set; }
+        public EAddressType Type { get; private set; }
+    }
+}
---
+++ WolneyStore.Domain/StoreContext/Commands/OrderCommands/Inputs/PlaceOrderCommand.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidator.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolneyStore.Domain.StoreContext.Commands.OrderCommands.Inputs
+{
+    public class PlaceOrderCommand 
+    {
+        public PlaceOrderCommand()
+        {
+            OrderItems = new List<OrderItemCommand>();
+        }
+
+        public Guid Customer { get; set; }
+        public IList<OrderItemCommand> OrderItems { get; set; }
+    }
+
+    public class OrderItemCommand
+    {
+        public Guid Product { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
